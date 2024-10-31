0c6f38fe4d60a05c3451b3fcb5f09e926a512578
leadermalang <hassanali5120@gmail.com>
2024-10-31 23:26:51 +0500

    updated contract address

--- client/src/ConnectivityAssets/environment.js
+++ client/src/ConnectivityAssets/environment.js
@@ -1,2 +1,2 @@
-export const tokenAddress = "0x67A049FF23c69D24203bbE1BD56eE91d44F0004F";
+export const tokenAddress = "0x2e6579bEa8C306e10FD6bfD1FC1FD2a0eafcB157";
 export const usdtAddress = "0x55d398326f99059fF775485246999027B3197955";
